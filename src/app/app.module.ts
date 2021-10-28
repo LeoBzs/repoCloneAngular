0716280eca1441deded6038c29740d438f8c468c
LeoBzs <leobazacas@gmail.com>
2021-10-28 20:11:29 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { ProductListComponent } from './product-list/product-list.component';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import {productAlertComponents} from './product-alerts/productAlertComponent';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -25,10 +26,3 @@ import {productAlertComponents} from './product-alerts/productAlertComponent';
   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
