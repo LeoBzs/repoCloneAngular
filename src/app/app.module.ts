7e746f7a36cca1447ef05eeac2ee6991a758c26e
LeoBzs <leobazacas@gmail.com>
2021-10-29 14:11:49 -0300

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
   @NgModule({
     bootstrap: [ AppComponent ],
@@ -16,6 +17,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
       RouterModule.forRoot([
         { path: '', component: ProductListComponent },
         { path: 'products/:productId', component: ProductDetailsComponent },
+        { path: 'cart', component: CartComponent },
       ])
     ],
     declarations: [
