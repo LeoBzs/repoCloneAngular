75dcf039f1b10cdc7bc0a5751c973acd56cfd690
LeoBzs <leobazacas@gmail.com>
2021-10-31 20:15:50 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
   @NgModule({
     bootstrap: [ AppComponent ],
@@ -18,6 +19,7 @@ import { CartComponent } from './cart/cart.component';
         { path: '', component: ProductListComponent },
         { path: 'products/:productId', component: ProductDetailsComponent },
         { path: 'cart', component: CartComponent },
+        { path: 'shipping', component: ShippingComponent },
       ])
     ],
     declarations: [
@@ -26,6 +28,8 @@ import { CartComponent } from './cart/cart.component';
       ProductListComponent,
       ProductAlertsComponent,
       ProductDetailsComponent,
+      CartComponent,
+      ShippingComponent
     ],
   })
 export class AppModule { }
